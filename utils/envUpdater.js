be4ae501cc3730660b4de22a592aa52f719c0bac
cmlima <caio.moyses.lima@gmail.com>
2020-03-16 16:00:59 -0300

    Changing default favicon

--- utils/envUpdater.js
+++ utils/envUpdater.js
@@ -3,6 +3,6 @@ module.exports = (content, { appName }) => {
         'VUE_APP_SIMULATE_INSTALLED = false',
         'VUE_APP_DEVELOPMENT_MODE = false',
         `VUE_APP_TITLE = ${appName}`,
-        'VUE_APP_FAVICON = https://github.com/ijusplab/Identidade-Visual/blob/66a39374709693667bac2657a503badc471f616f/src/logos/incubadora/logo_incubadora_favicon.png'
+        'VUE_APP_FAVICON = https://raw.githubusercontent.com/vuejs/vue-cli/eda048a0fce9bdd3ddb423d8c5f58e1448acc897/docs/.vuepress/public/icons/favicon-16x16.png'
     ].join('\n') + content;
 }
