6d4007fdcdaee7c1c29bdf3521a5412f230922bc
murashi-sn <nsa.toshi.shi@gmail.com>
2020-07-11 16:14:39 +0900

    use type file(@types/google-apps-script).

--- generator/templates/src/server/Service.ts
+++ generator/templates/src/server/Service.ts
@@ -1,30 +1,9 @@
-/* eslint-disable  @typescript-eslint/ban-ts-ignore */
-/* eslint-disable  @typescript-eslint/no-explicit-any */
-
 'use strict';
 
-interface IPostData {
-  length: number;
-  type: string;
-  contents: string;
-  name: string;
-}
-
-interface IGetEvent {
-  queryString: string;
-  parameter: object;
-  parameters: object;
-  contextPath: string;
-  contentLength: number;
-  postData: IPostData;
-}
-
-function doGet(e: IGetEvent) {
-  //@ts-ignore
-  var html = HtmlService.createTemplateFromFile('index');
+function doGet(e: GoogleAppsScript.Events.DoGet) {
+  const html = HtmlService.createTemplateFromFile('index');
   return html.evaluate()
     .addMetaTag('viewport', 'width=device-width, initial-scale=1')
-    //@ts-ignore
     .setXFrameOptionsMode(HtmlService.XFrameOptionsMode.ALLOWALL)
     .setTitle(process.env.VUE_APP_TITLE)
     .setFaviconUrl(process.env.VUE_APP_FAVICON);
