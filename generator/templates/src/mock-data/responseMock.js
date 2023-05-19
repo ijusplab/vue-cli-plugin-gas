86c05caf4ccabb8ff29e6242b57e42f310d2a080
murashi-sn <nsa.toshi.shi@gmail.com>
2023-05-19 21:41:24 +0900

    chore: fix
    #6

--- generator/templates/src/mock-data/responseMock.js
+++ generator/templates/src/mock-data/responseMock.js
@@ -2,6 +2,7 @@ module.exports = {
   sampleFunction: {
     isSuccess: true,
     response: {
+      // Please make sure to pass a JSON string as the value of the response.
       mode: 'This is development mode.'
     }
   }
